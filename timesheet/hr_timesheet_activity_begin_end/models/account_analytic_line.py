5bf7a887cc8119bf98a0af69565f819880615c39
Christopher Rogos <christpoher.rogos@glueckkanja-gab.com>
2020-11-20 12:25:06 +0100

    add logging

--- timesheet/hr_timesheet_activity_begin_end/models/account_analytic_line.py
+++ timesheet/hr_timesheet_activity_begin_end/models/account_analytic_line.py
@@ -73,6 +73,8 @@ class AccountAnalyticLine(models.Model):
 
     @api.depends("time_start", "time_stop")
     def onchange_hours_start_stop(self):
+         _logger.info("Triggered onchange_hours_start_stop")
+           
         start = timedelta(hours=self.time_start)
         stop = timedelta(hours=self.time_stop)
         if stop < start:
