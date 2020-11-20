bf2d0bb792aa886aef3a3b84bbda8f3e742343fc
Christopher Rogos <christpoher.rogos@glueckkanja-gab.com>
2020-11-20 12:07:57 +0100

    add logging

--- timesheet/hr_timesheet_activity_begin_end_addon/models/account_analytic_line.py
+++ timesheet/hr_timesheet_activity_begin_end_addon/models/account_analytic_line.py
@@ -33,14 +33,20 @@ class AccountAnalyticLine(models.Model):
              
     def _update_datetime_start(self):
         _logger.info("Triggered _update_datetime_start")
+        _logger.info(self)
         for rec in self:
             start = rec.datetime_start - datetime.combine(rec.datetime_start.date(), time(0))
             stop = rec.datetime_stop - datetime.combine(rec.datetime_stop.date(), time(0))
-
+            
             rec.unit_amount = (stop - start).seconds / 3600
+            _logger.info("Write unit_amount")
             rec.time_start = start.total_seconds() / 3600
+            _logger.info("Write time_start")
             rec.time_stop = stop.total_seconds() / 3600
+            _logger.info("Write time_stop")
             rec.date = rec.datetime_start.date()
+
+        _logger.info("Completed _update_datetime_start")
          
     def _update_datetime_stop(self):
         _logger.info("Triggered _update_datetime_stop")
