b6166b5859f7307f8245a36c2eb88914294c284f
Christopher Rogos <christpoher.rogos@glueckkanja-gab.com>
2020-12-03 17:23:34 +0100

    Fix export admin

--- setup/ir_exports_admin/__manifest__.py
+++ setup/ir_exports_admin/__manifest__.py
@@ -3,18 +3,20 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    "name": "gkgab - exports",
+    "name": "Export Admin",
     "version": "14.0.0.0.0",
     "author": "glueckkanja-gab",
     "license": "AGPL-3",
-    "category": "Utilities",
+    "category": "Productivity",
     "depends": [],
     "website": "",
     "data": [
         "views/ir_exports.xml",
         "views/ir_exports_line.xml",
-        'data/ir_actions_act_window.xml',
-        'data/ir_ui_menu.xml'
+        "data/ir_actions_act_window.xml",
+        "data/ir_ui_menu.xml",
+        "data/ir.exports.csv",
+        "data/ir.exports.line.csv",
     ],
     "installable": True,
     "auto_install": False,
