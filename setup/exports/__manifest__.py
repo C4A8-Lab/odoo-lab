62873e3646efae82758bb18978dcfc9fa0f64003
Christopher Rogos <christpoher.rogos@glueckkanja-gab.com>
2020-12-03 14:29:29 +0100

    add exports views

---
+++ setup/exports/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2015 Camptocamp SA - Guewen Baconnier
+# Copyright 2017 Tecnativa, S.L. - Luis M. Ontalba
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "gkgab - exports",
+    "version": "14.0.0.0.0",
+    "author": "glueckkanja-gab",
+    "license": "AGPL-3",
+    "category": "Utilities",
+    "depends": [],
+    "website": "",
+    "data": [
+        "views/ir_exports.xml",
+        "views/ir_exports_line.xml"
+    ],
+    "installable": True,
+    "auto_install": False,
+}
