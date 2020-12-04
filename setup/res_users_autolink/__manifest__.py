0cba996b750f227298381bf1c5ff668931b851a9
Christopher Rogos <christpoher.rogos@glueckkanja-gab.com>
2020-12-04 14:14:08 +0100

    automatic link new users to employees

---
+++ setup/res_users_autolink/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2015 Camptocamp SA - Guewen Baconnier
+# Copyright 2017 Tecnativa, S.L. - Luis M. Ontalba
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Link AAD users to employees",
+    "version": "14.0.0.0.0",
+    "author": "glueckkanja-gab",
+    "license": "AGPL-3",
+    "category": "Productivity",
+    "depends": ["hr.employees"],
+    "website": "",
+    "data": [
+    ],
+    "installable": True,
+    "auto_install": False,
+}
